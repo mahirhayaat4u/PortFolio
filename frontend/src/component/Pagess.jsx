4d226b21e11b3a7cab64226c01da36d6a42aa91d
MAHIR HAYAAT <89286205+mahirhayaat4u@users.noreply.github.com>
2026-03-19 19:35:54 +0530

    Remove unused SVG icon components and update link

--- frontend/src/component/Pagess.jsx
+++ frontend/src/component/Pagess.jsx
@@ -22,69 +22,7 @@ import {
 } from "react-icons/si";
 import { Briefcase } from "lucide-react";
 
-// Using inline SVGs to avoid dependencies for a single-file setup.
-// const MenuIcon = (props) => (
-//   <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-//     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
-//   </svg>
-// );
 
-// const CloseIcon = (props) => (
-//   <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-//     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-//   </svg>
-// );
-
-// const MailIcon = (props) => (
-//   <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-//     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
-//   </svg>
-// );
-
-// const PhoneIcon = (props) => (
-//     <svg {...props} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-//         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
-//     </svg>
-// );
-
-// const LinkedInIcon = (props) => (
-//     <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-//         <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
-//     </svg>
-// );
-
-// const GitHubIcon = (props) => (
-//     <svg {...props} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-//         <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
-//     </svg>
-// );
-
-// // -- SKILL ICONS --
-// const ReactIcon = (props) => (
-//     <svg {...props} viewBox="-10.5 -9.45 21 18.9" fill="none" xmlns="http://www.w3.org/2000/svg">
-//         <circle cx="0" cy="0" r="2" fill="currentColor"></circle>
-//         <g stroke="currentColor" strokeWidth="1" fill="none">
-//             <ellipse rx="10" ry="4.5"></ellipse>
-//             <ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse>
-//             <ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse>
-//         </g>
-//     </svg>
-// );
-// const NodeIcon = (props) => (
-//     <svg {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-//         <path d="M11.43.43a2 2 0 00-1.07.33L3.8 4.41a2 2 0 00-1.07 1.76v11.65a2 2 0 001.07 1.76l6.56 3.65a2 2 0 001.99 0l6.56-3.65a2 2 0 001.07-1.76V6.17a2 2 0 00-1.07-1.76l-6.56-3.65a2 2 0 00-.92-.33zm-.86 7.5l-3.41-1.9 4.3-2.43 3.42 1.9-4.31 2.43zm-4.48 4.47l3.42 1.9v3.8l-4.3-2.38v-3.32zm1.07.6l3.41-1.9 3.42 1.9-3.42 1.9-3.41-1.9zm7.8-2.5l-3.41 1.9v4.8l4.3-2.43V9.97z" fill="currentColor"/>
-//     </svg>
-// );
-// const JsIcon = (props) => (
-//     <svg {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-//         <path d="M21 23h-3.66l-2.93-3.72a2.33 2.33 0 01-.4-1.2v-6.32h5.08v2.1H16.2v4.11l2.58 3.32H21zm-8.85-2.51c-1.38.9-2.92 1.41-4.62 1.41a6.44 6.44 0 01-6.44-6.44A6.44 6.44 0 0112.1 8.52V10.7c-1.14-.6-2.48-.95-3.89-.95-2.6 0-4.71 2.11-4.71 4.71s2.1 4.72 4.7 4.72c1.33 0 2.57-.56 3.44-1.46l.73.98z" fill="currentColor"/>
-//     </svg>
-// );
-// const TailwindIcon = (props) => (
-//     <svg {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-//         <path d="M12.001 4.8c-3.149 0-5.699 2.55-5.699 5.7 0 2.22 1.25 4.14 3.05 5.02.43.21.65.69.54 1.14-.12.45-.55.74-1.02.74-.29 0-.57-.12-.78-.33a.998.998 0 00-1.4.15 1.001 1.001 0 00-.14 1.41c1.13 1.13 2.78 1.83 4.58 1.83 3.149 0 5.699-2.55 5.699-5.7s-2.55-5.7-5.7-5.7zm0-2.4c4.468 0 8.099 3.63 8.099 8.1 0 4.47-3.63 8.1-8.1 8.1s-8.099-3.63-8.099-8.1c0-4.47 3.63-8.1 8.1-8.1zM4.801 12c0-3.15 2.55-5.7 5.7-5.7s5.699 2.55 5.699 5.7c0 2.22-1.25 4.14-3.05 5.02-.43.21-.65.69-.54 1.14.12.45.55.74 1.02.74.29 0 .57-.12.78-.33a.998.998 0 001.4-.15c.34-.34.34-.9 0-1.24-1.13-1.13-2.78-1.83-4.58-1.83-3.149 0-5.699-2.55-5.699-5.7z" fill="currentColor"/>
-//     </svg>
-// );
 // -- ICONS --
 const MenuIcon = (props) => <FaBars {...props} />;
 const CloseIcon = (props) => <FaTimes {...props} />;
@@ -331,7 +269,7 @@ const Hero = () => {
             custom={3}
           >
             <motion.a
-              href="https://drive.google.com/file/d/1oIbmgZYvo3qO6jvU1ASKNg3hE22GKkN6/view?usp=sharing" // Placeholder link
+              href="https://drive.google.com/file/d/1gM1xVtpmlG6NzOSzYKHecwzkrJTuabxp/view?usp=sharing" // Placeholder link
               download
               className="bg-cyan-500 hover:bg-cyan-600 text-white font-bold py-3 px-6 rounded-lg shadow-lg transition-all duration-300 transform hover:scale-105"
               whileHover={{ scale: 1.05 }}
